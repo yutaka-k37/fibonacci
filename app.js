dfd07094bdd36c9d59c61a5b33b2164e9e2f8412
Yutaka Kobayashi <made.in.yutaka@gmail.com>
2020-02-24 14:56:38 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function fibonacci(number) {
+  if (number === 0) return 0
+  if (number === 1) return 1
+  return fibonacci(number - 1) + fibonacci(number - 2)
+}
+
+const length = 40
+for (let i = 1; i <= length; i++) {
+  console.log(fibonacci(i))
+}
